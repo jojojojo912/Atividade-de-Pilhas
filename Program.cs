e7d896b9333128b3c7c72863732d8748ce320c9c
joaopedrodesouza <joaopedrodesouza424@gmail.com>
2026-05-17 19:11:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class Produto
+{
+    public int CodigoProduto;
+    public string NomeProduto;
+    public double Preco;
+    public int QuantidadeEstoque;
+
+    public void ReceberDados()
+    {
+        Console.Write("Código do produto: ");
+        CodigoProduto = int.Parse(Console.ReadLine());
+        Console.Write("Nome do produto: ");
+        NomeProduto = Console.ReadLine();
+        Console.Write("Preço: ");
+        Preco = double.Parse(Console.ReadLine());
+        Console.Write("Quantidade inicial: ");
+        QuantidadeEstoque = int.Parse(Console.ReadLine());
+    }
+
+    public void AdicionarEstoque()
+    {
+        Console.Write("Quantidade a adicionar: ");
+        int qtd = int.Parse(Console.ReadLine());
+        QuantidadeEstoque += qtd;
+    }
+
+    public void RemoverEstoque()
+    {
+        Console.Write("Quantidade a remover: ");
+        int qtd = int.Parse(Console.ReadLine());
+        if (qtd <= QuantidadeEstoque)
+            QuantidadeEstoque -= qtd;
+        else
+            Console.WriteLine("Estoque insuficiente.");
+    }
+
+    public void MostrarProduto()
+    {
+        Console.WriteLine($"\n[Produto] Código: {CodigoProduto} | Nome: {NomeProduto} | Preço: R${Preco:F2} | Estoque: {QuantidadeEstoque}");
+    }
+}
+
+public class ProgramEx3
+{
+    public static void Main()
+    {
+        Produto p = new Produto();
+        p.ReceberDados();
+        p.MostrarProduto();
+        p.AdicionarEstoque();
+        p.MostrarProduto();
+        p.RemoverEstoque();
+        p.MostrarProduto();
+    }
+}
